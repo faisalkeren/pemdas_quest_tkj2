2bbe90a66d458e253be4e9f6208d680868f1e2d5
faisalkeren <125232086+faisalkeren@users.noreply.github.com>
2023-02-13 09:50:08 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,86 @@
+# BUATLAH PROGRAM MENGHITUNG LUAS BANGUN DATAR MENGHITUNG fungsi
+
+
+
+def luas_s(alas, tinggi):
+    luas =1/2 * alas * tinggi 
+    print("luas segitiga adalah", luas)
+
+luas_s(5,2)
+
+
+def luas_pp(panjang, lebar):
+    luas = panjang * lebar
+    print("luas persegi panjang", luas)
+
+luas_pp(6,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ latihan_fungsi.py
@@ -0,0 +1,41 @@
+'''fungsi pada pyhton di definisakan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("assalamualaikum !! akhi ukhty")
+
+salam()
+
+def takbir():
+    print("allahuakbarrr")
+
+takbir()
+takbir()
+takbir()
+takbir()
+takbir()
+takbir()
+takbir()
+takbir()
+takbir()
+
+"""fungsi dengan parameter"""
+"""def nama_fungsi(p1,p2)"""
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya", nama)
+
+#buatlah 3 parameter
+say_hi("jawir")
+say_hi("oncom")
+
+def perkenalan (nama, alamat, umur):
+
+    print("nama saya: ", nama)
+    print("alamat saya:", alamat)
+    print("umur saya:", umur)
+
+perkenalan("amin","kampung durian runtuh",49)
+
+
